00f93116fea91acfd8720168165f687435d901f6
paras603 <parasmanirai1@gmail.com>
2023-08-21 12:22:53 +0545

    initiating view model factory

---
+++ app/src/main/java/com/mastercoding/contactmanager/ViewModel/UserViewModelFactory.kt
@@ -0,0 +1,14 @@
+package com.mastercoding.contactmanager.ViewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.mastercoding.contactmanager.room.UserRepository
+
+class UserViewModelFactory(private val repository: UserRepository): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(UserViewModel::class.java)){
+            return UserViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown View Model Class")
+    }
+}
